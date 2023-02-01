6e5767e6ecca9c91bd33c508418b6c766f5df447
hekaruowo <nikita.chepik.owo@gmail.com>
2023-02-01 19:28:06 +0300

    ss

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,20 @@ import {Todolist} from "./Todolist";
 function App() {
     return (
         <div className="App">
-            <Todolist />
-            <Todolist />
-            <Todolist />
+            <Todolist title = "What to learn" tasks={tasks1}/>
+            <Todolist title = "Songs" tasks={tasks2}/>
         </div>
     );
 }
 
+const tasks1 = [
+    { id: 1, title: "HTML&CSS", isDone: true },
+    { id: 2, title: "JS", isDone: true },
+    { id: 3, title: "ReactJS", isDone: false }
+]
+const tasks2 = [
+    { id: 1, title: "Hello world", isDone: true },
+    { id: 2, title: "I am Happy", isDone: false },
+    { id: 3, title: "Yo", isDone: false }
+]
 export default App;
